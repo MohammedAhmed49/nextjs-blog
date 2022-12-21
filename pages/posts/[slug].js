3dbdc78e1c955bba69e8458bf5c52bd3651f1abd
unknown <mohammed_ahmed4949@yahoo.com>
2022-12-22 01:56:02 +0200

    Added Markdown to post details

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,32 @@
 import React from "react";
 import PostContent from "../../components/posts/post-details/PostContent";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-const SinglePostPage = () => {
-  return <PostContent />;
+const SinglePostPage = (props) => {
+  console.log(props.post);
+  return <PostContent post={props.post} />;
 };
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+  };
+}
+
+export function getStaticPaths() {
+  const postFilenames = getPostsFiles();
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ""));
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
+}
 
 export default SinglePostPage;
